0641f8d5c7319a993a1d71b8250c263cfc39fe7e
chenxz <563859095@qq.com>
2021-05-27 14:48:36 +0800

    commit

--- app/src/main/java/com/cxz/imdemo/receiver/CustomPushMessageReceiver.kt
+++ app/src/main/java/com/cxz/imdemo/receiver/CustomPushMessageReceiver.kt
@@ -1,6 +1,8 @@
 package com.cxz.imdemo.receiver
 
 import android.content.Context
+import android.content.Intent
+import com.cxz.imdemo.ui.MainActivity
 import io.rong.push.PushType
 import io.rong.push.notification.PushMessageReceiver
 import io.rong.push.notification.PushNotificationMessage
@@ -17,6 +19,23 @@ class CustomPushMessageReceiver : PushMessageReceiver() {
         pushType: PushType?,
         notificationMessage: PushNotificationMessage?
     ): Boolean {
+        if (notificationMessage?.sourceType != PushNotificationMessage.PushSourceType.FROM_ADMIN) {
+            val targetId = notificationMessage?.targetId ?: ""
+            //10000 为 Demo Server 加好友的 id，若 targetId 为 10000，则为加好友消息，默认跳转到 NewFriendListActivity
+            if (targetId == "10000") {
+                val intentMain = Intent(context, MainActivity::class.java)
+                intentMain.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                val intentNewFriend = Intent(context, MainActivity::class.java)
+                intentNewFriend.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                val intentArray = arrayOf(intentMain, intentNewFriend)
+                context?.startActivities(intentArray)
+                return true
+            } else {
+                val intentMain = Intent(context, MainActivity::class.java)
+                intentMain.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                context?.startActivity(intentMain)
+            }
+        }
         return super.onNotificationMessageClicked(context, pushType, notificationMessage)
     }
 
---
+++ app/src/main/java/com/cxz/imdemo/ui/NewFriendListActivity.kt
@@ -0,0 +1,12 @@
+package com.cxz.imdemo.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cxz.imdemo.R
+
+class NewFriendListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_friend_list)
+    }
+}
